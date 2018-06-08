7c3c8bef081b68b31fe797e88b67e92f09b63f62
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-08 15:37:54 +0300

    Update Task2_3.php

--- Task2/Task2_3.php
+++ Task2/Task2_3.php
@@ -1,31 +1,26 @@
 // /*Task2_3  В массиве А(N) первый положительный элемент и последний отрицательный элемент переставить местами.  
 // */
-$arr = array(88, 1, 139, 4, 3, 88, -7, 1, 88, 3, -123);
-$positiv;
-$negativ;
+$arr = array(-1, 77, 139, 4, 3, 88, -77, 1, 88, 10, 124);
+$k = count($arr) - 1;
 
 for($j = 0; $j < count($arr); $j++) {
   
-    if ($arr[$j] > 0) {
-		
-    	$positiv = $arr[$j];
-		
-		break;
+    if ($arr[$j] > 0 && $flagP == 0) {		
+    	$flagP = 1;
+    	$positiv = $j;
     } 
-
-}
-
-for($i = count($arr); $i >= 0; $i--) {
 	
-    		if ($arr[$i] < 0 ) {
-    			$negativ = $arr[$i];
-				$arr[$i] = $positiv;
-				$arr[$j] = $negativ; 
-				break; 
-   			} 
-		}
-for($f = 0; $f < count($arr); $f ++) {
+    if($arr[$k] < 0 && $flagN == 0){
+    	$flagN = 1;
+    	$negativ = $k;
+    }
+		   
+    if($flagN == 1 && $flagP == 1 ){
+	$x = $arr[$negativ];
+	$arr[$negativ] = $arr[$positiv];
+	$arr[$positiv] = $x; 
+    	break;
+    }
 
-echo 'index = '.$f.' value = '.$arr[$f].'<br>';
-
+    $k--;
 }
