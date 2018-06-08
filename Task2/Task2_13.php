50e9da19dca2306ec3581ee97212ee6ac04ae182
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-08 18:02:19 +0300

    Update Task2_13.php

--- Task2/Task2_13.php
+++ Task2/Task2_13.php
@@ -1,66 +1,39 @@
 //Task2_13 В  массиве  А(N)  найти  два  наименьших  элемента  и  два  наибольших элемента.  
 
-		$arr = array(190, 190, 19, 19, -3, 188, -700, -8, 188, 6, -11, -190);	
+	$arr = array(22, 21, 14, 19, 3, 10, 7, 8, 18, 6, 11);	
 	
-
-		$maxFirst = $arr[0];
-		$minFirst = $arr[0];
+	$maxFirst = $arr[0];
+	$minFirst = $arr[0];
 		
-		for ($i = 1; $i < count($arr); $i++) {
+	for ($i = 0; $i < count($arr); $i++) {
 
-			if ($arr[$i] > $maxFirst) {
-
-					$maxFirst = $arr[$i];
-			}	else if ($arr[$i] < $minFirst ) {
-
-			  		$minFirst = $arr[$i];
-					
-			}
+		if ($arr[$i] > $maxFirst) {
+			$maxFirst = $arr[$i];
+			
+		} else if ($arr[$i] < $minFirst ) {
+			  $minFirst = $arr[$i];					
 		}
-		//max
+	}
+	
+	for($j = 0; $j < count($arr); $j++)	{
+		if($arr[$j] != $maxFirst) {
+			$maxSecond = $arr[$j];
 
-		$maxSecond = $arr[0];
-		
-		for($j = 0; $j < count($arr); $j++){
-			if($arr[$j] == $maxFirst && $j != count($arr) - 1 ) {
-				
-				$maxSecond = $arr[$j + 1];
-
-			} else {
-				break;
-			}
+		} else if($arr[$j] != $minFirst) {
+			$minSecond = $arr[$j];
+		}
+			
 		}
 
-		for ($k = 1; $k < count($arr); $k++) {
+		for ($k = 0; $k < count($arr); $k++) {
 						
 			if ($arr[$k] > $maxSecond && $arr[$k] != $maxFirst) {
-				$maxSecond = $arr[$k];				
-			} 						
-		}
+				$maxSecond = $arr[$k];	
 
-		//min
-		
-		$minSecond = $arr[0];
-		
-		for($d = 0; $d < count($arr); $d++) {
-			if($arr[$d] == $minFirst && $d != count($arr) - 1) {
-				$minSecond = $arr[$d + 1];
-				
-			} else {
-				break;
-			}
-
-		}
-
-		for ($m = 1; $m < count($arr); $m++) {
-						
-			if ($arr[$m] < $minSecond && $arr[$m] != $minFirst) {
-				$minSecond = $arr[$m];				
-			} 
-
-		}
-
-
+			} else if ($arr[$k] < $minSecond && $arr[$k] != $minFirst) {
+				$minSecond = $arr[$k];				
+		} 			
+	}
 
 echo 'MAX1 = '.$maxFirst.'; MAX2 = '.$maxSecond.'<br>';	
 
