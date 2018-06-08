66dd33d342056e164cf0bec6c81f5cd5e9367403
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-08 16:54:19 +0300

    Update Task2_11.php

--- Task2/Task2_11.php
+++ Task2/Task2_11.php
@@ -1,39 +1,40 @@
 //Task2_11 В массиве А(N) найти первый отрицательный элемент, предшествующий максимальному элементу, и последний положительный элемент,  стоящий за минимальным элементом.  
 
 
-$arr = array(10, -2, -12, -4, 340, 4, 7, 8, -1, 88, -6);
+$arr = array(40, 1, 12, -4, 340, 4, -7, -8, 1, -88, 6);
 $max = $arr[0];
 $min = $arr[0];
-
 for($k = 0; $k < count($arr); $k++) {
   
-  if ($arr[$k] > $max ) {
+    if ($arr[$k] > $max ) {
+     	$max = $arr[$k];
+     	$indexMax = $k;
 
-    	$max = $arr[$k];
-    	$indexMax = $k;
-
-  } else if ($arr[$k] < $min ) {
-
-	    $min = $arr[$k];
-		$indexMin = $k;
-	}
+    } else if ($arr[$k] < $min ) {
+	 $min = $arr[$k];
+	 $indexMin = $k;
+    }
   
 }
 
-for($i = 0; $i < $indexMax; $i++){
+for($j = 0; $j < count($arr); $j++) {
+  
+    if ($arr[$j] < 0 && $flagN == 0 && $indexMax > $j) {		
+    	$flagN = 1;
+    	$negativ = $j;
+    } 
+	
+    if($arr[$k] > 0 && $flagP == 0 && $indexMin < $k){
+    	$flagP = 1;
+    	$positiv = $k;
+    }
+		   
+    if($flagN == 1 && $flagP == 1 ){
+    	break;
+    }
 
-	if($arr[$i] < 0){
-		echo 'Первый отрицательный элемент - '.$arr[$i].'<br>';
-		break;
-	} 
-
+    $k--;
 }
 
-for($j = count($arr); $j >= $indexMin - 1 ; $j--){
-
-	if($arr[$j] > 0){
-		echo 'Последний положительный элемент - '.$arr[$j].'<br>';
-		break;
-	} 
-
-}
+echo 'Первый отрицательный элемент - '.$arr[$negativ].'<br>';
+echo 'Последний положительный элемент - '.$arr[$positiv].'<br>';
