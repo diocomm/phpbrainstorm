f65b6ffb567ba2dd93f96c4c92cdc9b7c1308f6d
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 12:54:05 +0300

    Update Task2_34.php

--- Task2/Task2_34.php
+++ Task2/Task2_34.php
@@ -1,39 +1,43 @@
-//Task2_34 В массиве А(N) расположить элементы в порядке возрастания, затем вставить в него новые k чисел, не нарушая упорядоченности.  
+// //Task2_34 В массиве А(N) расположить элементы в порядке возрастания, затем вставить в него новые k чисел, не нарушая упорядоченности.  
 
-$arrA = array(0,12, 2, 32, -4, 5, 8, 8, 19, 28, 33, -41);
+$arrA = array(0,12, 2, 32, -4, 5, 8, 8, 19, 28, 33, -1);
 $p = 5;
 for($k = 0; $k < count($arrA); $k++){
 
 	for($i = 0; $i < count($arrA) - 1; $i++){
 
  		if($arrA[$i] > $arrA[$i + 1] ) {
-        $x = $arrA[$i + 1];
-        $arrA[$i + 1] = $arrA[$i];
-        $arrA[$i] = $x;
+			$x = $arrA[$i + 1];
+			$arrA[$i + 1] = $arrA[$i];
+			$arrA[$i] = $x;
 
 		}
  	}	
 }
 
 for($t = 0; $t < $p; $t++){
-  	$element = rand(0, 20);
+	$element = rand(0, 20);
 	
 	for($i = 0; $i < count($arrA); $i++) {
 
 		if ($element > $arrA[count($arrA) - 1]){	
-		  	array_push($arrA, $element);	
+			//array_push($arrA, $element);
+			$arrA[count($arrA)] = $element;
 		} else if ($arrA[$i] <= $element && $arrA[$i + 1] >= $element) {
 
 				$temp = $arrA[count($arrA) - 1];
 			     
 			    for ($r = count($arrA) - 1; $r > $i ;$r--) {
-		  	     	$arrA[$r] = $arrA[$r - 1];
+			     	$arrA[$r] = $arrA[$r - 1];
 			    }
 			     
 			    $arrA[$i + 1] = $element;
-			    array_push($arrA, $temp);		
-		  		break;
+
+			    $arrA[count($arrA)] = $temp;		
+				break;
 		}
 		
+			
+
 	}
 }
