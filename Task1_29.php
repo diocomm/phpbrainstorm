3ab91bcae169df2ac606d91f5a9056cacadda599
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 10:10:46 +0300

    Update Task1_29.php

--- Task1_29.php
+++ Task1_29.php
@@ -1,34 +1,32 @@
 //Task1_29 Для  каждого  числа  заданной  последовательности  натуральных   чисел   n0 ,n1,...,nm установить,  можно  ли  вычеркнуть  в  нем  некоторые  цифры, чтобы сумма оставшихся равнялась заданному числу к.  
 
 
-$arr = array();
+
 $findNumber = 4;
-$limit1 = 1;
-$limit2 = 1000;
+$limit1 = 10;
+$limit2 = 100;
 
 for($j = $limit1; $j < $limit2; $j++) {
  
-    $num = $j;
-    $arr = array();
+    $num1 = $j;
+   	$sum = 0;
 
-    while ($num != 0) {
-        $dig =  $num % 10;
-        $num = floor($num / 10);
-        array_push($arr, $dig);
+    while ($num1 != 0) {
+        $dig =  $num1 % 10;
+        $num1 = floor($num1 / 10);
+        $sum += $dig;
+     
+    } 
+    $num2 = $j;
+   	while ($num2 != 0) {
+        $dig2 =  $num2 % 10;
+        $num2 = floor($num2 / 10);
+        
+        if(($sum - $dig2) == $findNumber && $dig2 != 0){
+        
+        	echo 'Из числа '.$j.' можно вычеркнуть '.$dig2.' что бы получилось '.$findNumber.'<br>';;
+        }
+     
     } 
 
-    $sum = 0;
-    for ($i = 0; $i < count($arr); $i++){
-
-        $sum += $arr[$i];
-
-    }
-
-    for ($f = 0; $f < count($arr); $f++){
-        if($sum - $arr[$f] == $findNumber && $arr[$f] != 0){
-            echo 'Из записи числа '.$j.'- можно вычеркнуть '.$arr[$f].' что бы получилось = '.$findNumber.'<br>';          
-        }
-
-    }
-
-}
+echo $j.'<br>';
