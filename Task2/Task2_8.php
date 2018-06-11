f074e6955a6f722292caeb05ad81df654e0105bc
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 11:50:00 +0300

    Update Task2_8.php

--- Task2/Task2_8.php
+++ Task2/Task2_8.php
@@ -1,45 +1,29 @@
 // //Task2_8 В массиве А(N) поменять местами последний отрицательный элемент с максимальным элементом.  
 
-$arr = array(88, -11, 19, 4, 3, 88, 7, -8, 88, 6, 1);
+$arr = array(8, -11, 19, 4, 3, 188, 7, -8, 154, 6, 1);
 
 $max = $arr[0];
 $lastNegativ;
-
+$i = count($arr) - 1;
 for($k = 0; $k < count($arr); $k++) {
   
-  if ($arr[$k] > $max) {
+  	if ($arr[$k] > $max) {
 
-    $max = $arr[$k];
+   		$max = $arr[$k];
+   	 	$indexMax = $k;
+ 	}
 
-  }
+	if ($arr[$i] < 0 && $flagLast == 0){
+    	$flagLast = 1;
+    	$indexLast = $i;
+    }
 
+    $i--;
 }
-
-for($i = count($arr); $i >= 0; $i--) {
+	$x = $arr[$indexLast];
+	$arr[$indexLast] = $arr[$indexMax];
+	$arr[$indexMax] = $x; 
 	
-    if ($arr[$i] < 0 ) {
-
-    	$lastNegativ = $arr[$i];
-
-		for($i = 0; $i < count($arr); $i++) {
-			  
-			if ($arr[$i] == $max ) {
-
-				$arr[$i] = $lastNegativ;
-
-			} else if ($arr[$i] == $lastNegativ){
-
-  				$arr[$i] = $max;
-
-  			}
-  
-		}
- 	
- 		break; 
-
-    } 
-
-}
 
 for($f = 0; $f < count($arr); $f ++) {
 
