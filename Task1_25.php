c7d6c7ad5bdc7ba321e41f240bb03bd51f5e4a98
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 11:17:01 +0300

    Update Task1_25.php

--- Task1_25.php
+++ Task1_25.php
@@ -1,42 +1,25 @@
-//Task1_25 Среди      заданной      последовательности      натуральных      чисел    n0 ,n1,...,nm найти  сумму  и  количество  тех  чисел,  которые  равны  сумме  факториалов своих цифр. 
-
-$arr = array();
-$findNumber = 40585;
 $limit1 = 1;
 $limit2 = 50000;
-$sumFact = 0;
 
 for($j = $limit1; $j < $limit2; $j++) {
-    $findNumber = $j;
     $num = $j;
-    $arr = array();
+   	$sumFact = 0;
     while ($num != 0) {
 
         $dig =  $num % 10;
         $num = floor($num / 10);
-        array_push($arr, $dig);
+
+        $factorial = 1;
+	        for($k = 2; $k <=  $dig; $k++){
+	            $factorial *= $k;       
+	        }
+
+       	$sumFact += $factorial;
     } 
 
-
-    $sum = 0;
-
-    for ($i = 0; $i < count($arr); $i++){
-        $factorial = 1;
-        
-        for($k = 1; $k <=  $arr[$i]; $k++){
-            $factorial *= $k;
-        
-        }
-
-        $sum += $factorial;
+    if($sumFact == $j){
+    	$sumNumber += $sumFact;
+    	$countNumber++;
     }
-   
-    if($sum == $findNumber){
-
-    	$sumFact += $findNumber;
-    	echo $findNumber.'<br>';
-
-    }
-
 }
-echo 'Сумма '.$sumFact;
+echo 'Количество чисел = '.$countNumber.' : Сумма = '.$sumNumber.'<br>';
