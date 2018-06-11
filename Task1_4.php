6e97596f1f4d7fab85e0a0b41dfc3c1c2bc09347
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 09:30:18 +0300

    Update Task1_4.php

--- Task1_4.php
+++ Task1_4.php
@@ -1,24 +1,26 @@
 //Task1_4 Найти все четные четырехзначные числа, цифры которых следуют в порядке возрастания или убывания.
 
-for($i = 1000; $i < 9999; $i+=2) {
-	$arrNumber = array();
-	$num = $i;
-	$a = 10;
-	$b = 1;
-	 while ($num > 0) {//for($k = 0; $k < 4; $k++){
+for($i = 1000; $i <= 9999; $i += 2){
+    $num = $i;
+	$flagDif = true;
+            while (($num != 0) && $flagDif) {
+                $dig1 = $num % 10;
+                $num = floor($num / 10);
 
-        $digit = $num % $a / $b;
-        $num = $num - ($num % $a);
-       	array_push($arrNumber, $digit);
+                $tempNum = $num;
 
-        $a *= 10;
-        $b *= 10;
+	                while (($tempNum != 0) && $flagDif ) {
+	                	$dig2 = $tempNum % 10;
+	                    if ($dig2 <= $dig1) {
+	                    	$flagDif = false;
+	                    } 
+	                    	
+	                    $tempNum = floor($tempNum / 10);
+	                }
+            }
 
-  }
-  
-    if ($arrNumber[0] > $arrNumber[1] && $arrNumber[1] > $arrNumber[2] && $arrNumber[2] > $arrNumber[3])  {
+            if ($flagDif) {
+                echo $i.'<br>';
+            } 
 
-      	echo $i.'<br>';
-
-  	} 
-}
+} 
