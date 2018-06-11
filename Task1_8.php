14f49f1ae02696a2710616d1d034508a2efdadd2
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 09:27:52 +0300

    Update Task1_8.php

--- Task1_8.php
+++ Task1_8.php
@@ -1,22 +1,26 @@
 //Task1_8 Получить все четырехзначные целые числа, в записи которых нет одинаковых цифр.
 
-for($num = 1000; $num < 9999; $num++){
+for($i = 1000; $i <= 9999; $i++){
+    $num = $i;
+	$flagDif = true;
+            while (($num != 0) && $flagDif) {
+                $dig1 = $num % 10;
+                $num = floor($num / 10);
 
-$num4 = ($num % 100) % 10; //4
-$num3 = floor($num % 100 / 10);
-$num2 = floor($num / 100) % 10;
-$num1 = floor(floor($num / 100) / 10);
+                $tempNum = $num;
 
+	                while (($tempNum != 0) && $flagDif ) {
+	                	$dig2 = $tempNum % 10;
+	                    if ($dig2 == $dig1) {
+	                    	$flagDif = false;
+	                    } 
+	                    	
+	                    $tempNum = floor($tempNum / 10);
+	                }
+            }
 
+            if ($flagDif) {
+                echo $i.'<br>';
+            } 
 
-if (($num4 != $num3) && 
-	($num2 != $num1) && 
-	($num4 != $num2) &&
-	($num3 != $num1) &&
-	($num2 != $num3) &&
-	($num1 != $num4) ) {
-
-	echo $num.'<br>';
-}
-
-}
+}  
