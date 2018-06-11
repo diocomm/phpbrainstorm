a9ec10b443f7aac2923fa7e268297a00454fa683
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 12:32:14 +0300

    Update Task2_14.php

--- Task2/Task2_14.php
+++ Task2/Task2_14.php
@@ -1,21 +1,28 @@
-$arr = array(19, 10, 3, 12, -3, 18, -7, -8, 18, 6, -11, -19);	
+arr = array(19, 10, 3, 12, -3, 18, -7, -8, 18, 6, -11, -19);	
 $arrNeg = array();
 $arrPos = array();
 $arrOdd = array(); 
 $arrEven = array(); 
+$k = 0;
+$j = 0;
+$r = 0;
+$d = 0;
 
 for($i = 0; $i < count($arr); $i++){
 	if($arr[$i] < 0){
-		  array_push($arrNeg, $arr[$i]);
+		  $arrNeg[$k] = $arr[$i];
+		  $k++;
 	} else {
-	  	array_push($arrPos, $arr[$i]);
+	  	$arrPos[$r] = $arr[$i];
+	  	$r++;
 	}
   
 	if($arr[$i] % 2 == 0){
-  		array_push($arrOdd, $arr[$i]);
+  		$arrOdd[$j] = $arr[$i];
+  		$j++;
 	} else {
-  		array_push($arrEven, $arr[$i]);
+  		$arrEven[$d] = $arr[$i];
+  		$d++;
 	}
-
 
 }
