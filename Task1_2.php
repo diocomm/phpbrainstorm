69e5101ef15d8e2c411f6a96663be062d6818f0c
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-11 10:17:03 +0300

    Update Task1_2.php

--- Task1_2.php
+++ Task1_2.php
@@ -1,25 +1,19 @@
+
 //Task1_2  Получить  все  четырехзначные  числа,  сумма  цифр  которых  равна заданному числу  n.
 
 $findNumber = 5;
 
 for($i = 1000; $i < 9999; $i++){
-    $num = $i;
-    $a = 10;
-    $b = 1;
+  	$a = $i;
     $sum = 0;
 
+    while ($a != 0){
 
-    for($k = 0; $k < 4; $k++){
-
-        $digit = $num % $a / $b;
-        $num = $num - ($num % $a);
-       
-        $a *= 10;
-        $b *= 10;
-
-        $sum += $digit;
-
-    }
+	    $dig = $a % 10;
+		$a = floor($a / 10);
+		$sum += $dig;
+	      
+	}
 
     if ($sum == $findNumber){
         echo $i.'</br>';
