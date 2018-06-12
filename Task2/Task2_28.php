1c52c149a236c4586aa8a579c25a24a094f80be1
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-12 15:16:03 +0300

    Update Task2_28.php

--- Task2/Task2_28.php
+++ Task2/Task2_28.php
@@ -1,36 +1,45 @@
 //Task2_28. Из  массива  А(N)  удалить  элементы,  стоящие  за  первым  максимальным элементом, количество цифр которых равно  k.  
 
-$arr = array(122, 2, 3, 4122, 5, 6, 88, 98, 199, 288, 336, 411, 555,888,898,222,222,333,222,555,888,898,222,222,333,222,1111,1121);
+$arr = array(122, 2, 3, 4121, 5, 6, 88, 98, 199, 288, 336, 411, 555,888,898,222,222,333,222,555,888,898,222,222,333,222,1111,1121);
 $arrB = array();
-$k = 3;//k элементов
+$n = 3;//k элементов
 
 $max = $arr[0];
+$indexMax = 0;
+
 for($p = 0; $p < count($arr); $p++) {
   
 	if ($arr[$p] > $max ) {
-
-  		$max = $arr[$p];
+ 		$max = $arr[$p];
+  		$indexMax = $p;
+  		
   	} 	
 }
+	$countArr = count($arr);
 
-for($d = 0; $d < count($arr); $d++) {
- 	$arrLimit = count($arr);
-	if ($arr[$d] == $max ) {
+ 	for($j = $indexMax + 1; $j < $countArr; $j++){		
+ 		$count = 0;
+		$a = $arr[$j]; 
+	
+	  	while ($a != 0){
+			$a = floor($a / 10);
+			$count++;				
+		}
+		
+		if($count == $n){
+			$countDel++;
+			$countArr--;
 
-      for($j = $d + 1; $j < $arrLimit; $j++) {
-          $count = 0;
-          $a = $arr[$j]; 
+			for($i = $j; $i < $countArr; $i++) {
+            			$arr[$i] = $arr[$i + 1];        		
+     	 		}
 
-          while ($a != 0){
-            $a = floor($a / 10);
+     	 		$j--;
+		}
+	}
 
-            $count++;				
-          }
-
-        if($count == $k){
-            unset($arr[$j]);
-
-        }				
-      }
-  	} 	
+for($h = 0; $h < count($arr) - $countDel; $h++){
+	$arr2[$h] = $arr[$h];
 }
+
+$arr = $arr2;
