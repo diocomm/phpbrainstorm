0a2311d3f6e62a7db3306ed239e4425e81d81fb3
diocomm <36219989+diocomm@users.noreply.github.com>
2018-06-12 15:33:48 +0300

    Update Task2_25.php

--- Task2/Task2_25.php
+++ Task2/Task2_25.php
@@ -1,5 +1,7 @@
-$arrA = array(8, 11, 19, -4, 123, 88, 18, 99, 88, 6, 11);
+// //Task2_25 В массив А(N) вставить после первого максимального элемента  k   минимальных элементов массива.  
 
+$arrA = array(1, 2, 3, 4, 5, 88, 98, 19, 28, 33, 11);
+$arrB = array();
 $p = 15;
 $max = $arrA[0];
 $min = $arrA[0];
@@ -21,9 +23,21 @@ for($k = 0; $k < count($arrA); $k++) {
 for($i = 0; $i < count($arrA); $i++) {
 
 	if($arrA[$i] == $max) {
+
+		$c = 0;
+		for($j = $i + 1; $j < count($arrA); $j++){
+			$arrB[$c++] = $arrA[$j];
+		}
 	
-		for($h = $i + 1; $h <= ($p + $i); $h++){//insert min
+		for($h = $i + 1; $h <= ($p + $i); $h++){
 			$arrA[$h] = $min;
+		}
+
+		$b = 0;
+		for($m = $h; $m < (count($arrB) + $h); $m++){
+			$arrA[$m] = $arrB[$b++];
+		
 		}
 	}
 }
+
